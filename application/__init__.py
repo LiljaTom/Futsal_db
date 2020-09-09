3784020c73dfa5d5deb210cecf434e821c5688d9
Tomi Lilja <tomi.lilja@helsinki.fi>
2020-09-09 19:03:24 +0300

    Add initial auth functionality

--- application/__init__.py
+++ application/__init__.py
@@ -8,17 +8,34 @@ from flask_sqlalchemy import SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///futsal.db"
 app.config["SQLALCHEMY_ECHO"] = True
 
-#db
 db = SQLAlchemy(app)
 
-#views
+
 from application import views
+
+#teams
+from application.teams import models
 from application.teams import views
 
-#models
-from application.teams import models
+#auth
+from application.auth import models
+from application.auth import views
 
-#create dbb tables
+# Login
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+#create db tables
 db.create_all()
-
-from application import views
